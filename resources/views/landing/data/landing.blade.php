2fdb720fe7568457db7f58597d350bce163c9e55
ilhamibnu <e41210178@student.polije.ac.id>
2024-01-21 09:05:09 +0700

    .

--- resources/views/landing/data/landing.blade.php
+++ resources/views/landing/data/landing.blade.php
@@ -36,7 +36,7 @@
 
 
 
-                     <a href="/detail-kost/{{ $data->id }}/#detail-kost" target="_blank"><img height="500px" width="500px" src="{{ asset('fotokost/' . $data->foto) }}" alt="IMG-PRODUCT"></a>
+                     <img height="500px" width="500px" src="{{ asset('fotokost/' . $data->foto) }}" alt="IMG-PRODUCT">
 
                     <a href="/detail-kost/{{ $data->id }}/#detail-kost" target="_blank" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04">
                         Detail
@@ -90,7 +90,7 @@
 
                             @endif
 
-                                <a href="/detail-kost/{{ $data['data']->kost->id }}/#detail-kost" target="_blank"><img height="500px" width="500px" src="{{ asset('fotokost/' . $data['data']->kost->foto) }}" alt="IMG-PRODUCT"></a>
+                               <img height="500px" width="500px" src="{{ asset('fotokost/' . $data['data']->kost->foto) }}" alt="IMG-PRODUCT">
 
 
 
